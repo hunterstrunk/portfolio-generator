c16768ac76049ca9824f96fa2b1f32e93a357d4c
hunterstrunk <hunter@strunkfam.com>
2021-01-28 20:27:51 -0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
